d4df7e1e137c9713cbaca56993cfd4bbdc3128c9
renato <renato@teste.com>
2025-05-24 14:33:48 -0300

    configure_deploy

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "bookstore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "bookstore" / "templates"],
+        'DIRS': [os.path.join(BASE_DIR, 'bookstore', 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
